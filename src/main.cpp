2a3bbba198b75c891e796d3a002a750d6af3a4a2
danielsantos-ti <sysdsantos@gmail.com>
2025-02-12 17:46:45 -0300

    Remove linhas duplicadas e corrige a posição da configuração do pino do loop para o setup

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,11 @@ int sensorACS = A0;
 // Variável armazena valor lido
 int armazenaValorAnalogico = 0;
 
-
 // Função de configuração
 void setup() {
+
+  // Configura o pino do sensor de corrente como entrada
+  pinMode(sensorACS, INPUT);
 
   // Inicializa a comunicação serial com baud rate de 9600
   Serial.begin(9600);
@@ -23,21 +25,13 @@ void loop() {
   // Lê o valor do sensor de corrente
   armazenaValorAnalogico = analogRead(sensorACS);
 
-  // Configura o pino do sensor de corrente como entrada
-  pinMode(sensorACS, INPUT);
-  
-  // Lê o valor do sensor de corrente
-  armazenaValorAnalogico = analogRead(sensorACS);
-
+  // Calcula a corrente em Ampere
   float corrente = (armazenaValorAnalogico - 512) / 66.0;
 
+  // Imprime o valor da corrente no monitor serial
   Serial.print("Corrente: ");
   Serial.print(corrente);
   Serial.println(" Ampere: ");
   delay(1000);
-  
-  // Chama a função que imprime o valor lido pelo sensor de corrente
-  // imprimeValorAnalogico();
-
 
 }
