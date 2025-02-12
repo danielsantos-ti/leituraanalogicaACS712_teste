e4645f9d643be2f42020f3ea982811f00c662c4d
danielsantos-ti <sysdsantos@gmail.com>
2025-02-12 09:50:19 -0300

    Adiciona comentários ao código

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+// Chama biblioteca Arduino.h
 #include <Arduino.h>
 
 // Pino de entrada do sensor de corrente ACS712 de 30A
@@ -6,18 +7,37 @@ int pinoAnalogico = A0;
 // Variável armazena valor lido
 int armazenaValorAnalogicoLino = 0;
 
+
+// Função que imprime o valor lido pelo sensor de corrente
+void imprimeValorAnalogico(){
+
+  // imprime o valor lido pelo sensor de corrente
+  Serial.println(armazenaValorAnalogicoLino);
+  // Intervalo de 1 segundo
+  delay(1000);
+
+}
+
+// Função de configuração
 void setup() {
 
+  // Inicializa a comunicação serial com baud rate de 9600
   Serial.begin(9600);
   
 }
 
+
+// Função de execução
 void loop() {
   
+  // Lê o valor do sensor de corrente
   armazenaValorAnalogicoLino = analogRead(pinoAnalogico);
 
+  // Configura o pino do sensor de corrente como entrada
   pinMode(pinoAnalogico, INPUT);
-  Serial.println(armazenaValorAnalogicoLino);
-  delay(1000);
+
+  // Chama a função que imprime o valor lido pelo sensor de corrente
+  imprimeValorAnalogico();
+  
 
 }
